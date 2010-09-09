56b9b25589184aaeb699dedc39d616c0c20edb82
Vidit Drolia <vidit@yahoo-inc.com>
2010-09-08 23:56:34 -0700

    p2:ruby working solution
    add - backtracking
    todo - clean up backtracking, its very messy right now

--- edit_distance/test/edit_distance_test.rb
+++ edit_distance/test/edit_distance_test.rb
@@ -1,1 +1,22 @@
-require 'edit_distance'
+require 'src/edit_distance'
+require 'test/unit'
+
+class EditDistanceTest < Test::Unit::TestCase
+  def setup
+    @ed = EditDistance.new
+  end
+  def test_edit_distance
+    l = "foo1"
+    t = "baz"
+    r = @ed.edit_distance_wrapper(l,t)
+    assert_equal(5,r[:cost])
+    l = "abcdefghijkl"
+    t = "bcdefghxlm"
+    r = @ed.edit_distance_wrapper(l,t)
+    assert_equal(7,r[:cost])
+    l = "abrackadbrea"
+    t = "abracadabra"
+    r = @ed.edit_distance_wrapper(l,t)
+    assert_equal(5,r[:cost])
+  end
+end
