b731b1dace5ae3503d3812908ec49f76e1a54469
Rodney Lopes Gomes <rodneygomes@gmail.com>
2010-09-09 13:54:23 -0700

    - by calculating the cost as we're going down any new path we can easily discard/prune entries quickly

--- edit_distance/src/edit_distance.pl
+++ edit_distance/src/edit_distance.pl
@@ -31,26 +31,24 @@ t3(A,B) :- cs(X), ins(A,X,B).
 % calculates the distance between two words in terms of characters
 % that are different
 diff([X|T1],[Y|T2],N) :- (X == Y,diff(T1,T2,N); diff(T1,T2,N2),N is N2 + 1), !.
-diff(X,[],N) :- length(X,N1), N is N1 * 3, !.
-diff([],X,N) :- length(X,N1), N is N1 * 3, !.
+diff(X,[],N) :- length(X,N1), N is N1, !.
+diff([],X,N) :- length(X,N1), N is N1, !.
 
 % pretty printing the converts the [[a,b,c]] back into [abc] which is easier
 % for the humanoids
 pp([],[]).
 pp([X|T],[H|T2]) :- atom_chars(H,X), pp(T,T2).
 
-calc(A,B,(C,R2)) :- edist(A,B,R,C), pp(R,R2).
+calc(A,B,(C,R2)) :- edist(A,B,R,0,C), pp(R,R2).
 
-edist(A,B,[],0) :- diff(A,B,0).
-edist(A,B,[X|R],C) :- t1(A,X), diff(A,B,N1), diff(X,B,N2), N2 < N1, edist(X,B,R,C1), C is C1 + 1.
-edist(A,B,[X|R],C) :- length(A,L1), length(B,L2), L1 > L2, t2(A,X), diff(A,B,N1), diff(X,B,N2), N2 < N1, edist(X,B,R,C1), C is C1 + 2.
-edist(A,B,[X|R],C) :- length(A,L1), length(B,L2), L1 < L2, t3(A,X), diff(A,B,N1), diff(X,B,N2), N2 < N1, edist(X,B,R,C1), C is C1 + 3.
+edist(A,B,[],_,0) :- diff(A,B,0).
+edist(A,B,[X|R],AC,C) :- CC is AC + 1, best(_,BC), CC < BC, t1(A,X), diff(A,B,N1), diff(X,B,N2), N2 < N1, edist(X,B,R,CC,CT), C is CT + 1.
+edist(A,B,[X|R],AC,C) :- CC is AC + 2, best(_,BC), CC < BC, t2(A,X), diff(A,B,N1), diff(X,B,N2), N2 < N1, edist(X,B,R,CC,CT), C is CT + 2.
+edist(A,B,[X|R],AC,C) :- CC is AC + 3, best(_,BC), CC < BC, t3(A,X), diff(A,B,N1), diff(X,B,N2), N2 < N1, edist(X,B,R,CC,CT), C is CT + 3.
 
 valid(B,R) :- best(B1,R1), R < R1, retractall(best(B1,R1)), asserta(best(B,R)), !.
 
 gen(A,B,_) :- calc(A,B,(C,R1)), valid(R1,C), fail.
 gen(_,_,(C,R)) :- best(R,C).
 
-calcall(A,B,R) :- atom_chars(A,A1), atom_chars(B,B1), init(A1,B1),
-                  asserta(best([], 999999)), gen(A1,B1,R), retractall(best(_,_)).
-
+calcall(A,B,R) :- atom_chars(A,A1), atom_chars(B,B1), init(A1,B1), asserta(best([], 999999)), gen(A1,B1,R), retractall(best(_,_)).
