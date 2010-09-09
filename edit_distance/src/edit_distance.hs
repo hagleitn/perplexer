316af0942b5a9f798b3b8a3007730a2395292076
Gunther Hagleitner <hagleitn@masteronly-lm.corp.yahoo.com>
2010-09-09 16:00:40 -0700

    clean up

--- edit_distance/src/edit_distance.hs
+++ edit_distance/src/edit_distance.hs
@@ -1,42 +1,47 @@
 import Data.List
 import Data.Array
-import System( getArgs )
+import System
 
 instance (Ord a) => Ix [a] where
   range (m,n) = map (\i -> [ undefined | _ <- [1..i]]) [(length m)..(length n)]
-  index (m,n) i = index (length m, length n) (length i)
-  inRange (m,n) i = inRange (length m, length n) (length i)
+  index (m,n) i = index (length m, length n) $ length i
+  inRange (m,n) i = inRange (length m, length n) $ length i
 
 editDistance :: (Num b, Ord b, Ord a) => (a -> b) -> (a -> b) -> (a -> a -> b) -> [a] -> [a] -> (b,[[a]])
 editDistance delCost insCost editCost v w =
   let
-    mkIns (x,(y:ys)) = (c+(insCost y), (y:s):r)
+    ins (x,(y:ys)) = (insCost y + c, (y:s):r)
       where (c,r@(s:rs)) = memoized_dist (x,ys)
 
-    mkDel ((x:xs),y) = (c+(delCost x), ss:r)
+    del ((x:xs),y) = (delCost x + c, ss:r)
       where
         (c,res) = memoized_dist (xs,y)
         r@((s:ss):rs) = map ((:) x) res
 
-    mkEdit ((x:xs),(y:ys))
+    edit ((x:xs),(y:ys))
         | x == y = (c,r)
-        | otherwise = (c+(editCost x y),(y:ss):r)
+        | otherwise = (editCost x y + c, (y:ss):r)
       where
-        (c,res) = memoized_dist (xs,ys)
+        (c,res) = memoized_dist (xs, ys)
         r@((s:ss):rs) = map ((:) x) res
     
     memoized_dist =
       let        
         dist ([],[]) = (0,[[]])
-        dist k@([],y) = mkIns k
-        dist k@(x,[]) = mkDel k
-        dist k  = minimum [ mkIns k, mkDel k, mkEdit k ]
+        dist k@([],y) = ins k
+        dist k@(x,[]) = del k
+        dist k  = minimum [ ins k, del k, edit k ]
       in
-        (fmap dist (array (([],[]),(v,w)) [ ((i,j),(i,j)) | i <- (tails v), j <- (tails w) ]) !)
+        (fmap dist (array (([],[]),(v,w)) [ ((i,j),(i,j)) | i <- tails v, j <- tails w ]) !)
 
-    rev (a,b) = (a, reverse b)
+    rev (a,b) = (a,reverse b)
   in
     (rev . memoized_dist) (v,w)
 
 editDist :: (Num b, Ord b, Ord a) => [a] -> [a] -> (b,[[a]])    
 editDist v w = editDistance (const 2) (const 3) (\i j -> 1) v w
+
+main :: IO ()
+main = do
+  (a:b:[]) <- getArgs
+  print $ editDist a b
