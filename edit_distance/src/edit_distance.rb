31266c8775b53d9c99354c6c2d835307dc0a1973
vidit <viditdrolia@gmail.com>
2010-09-09 22:55:26 -0700

    p2:ruby fixed backtracking (again)
    fix - now we backtrack to cells only if the difference between the two
    cell values is the same as the cost for the jump - big thanks to Gunther
    for pointing out my stupidity
    todo - clean, as usual

--- edit_distance/src/edit_distance.rb
+++ edit_distance/src/edit_distance.rb
@@ -29,14 +29,14 @@ class EditDistance
       return min 
     end
   end
-
   def transform(op,l,t,i,j,table)
-    t[0,j] + l[i,l.size]
+    j = t.size if j > t.size
+    s = t[0,j] + l[i,l.size] 
   end
 
   #TODO clean this mess up
-  #pseudocode: start form the first cell, keep jumping to the adjacent cell with
-  #minimum cost (difference between cell values). if you hit a wall, then you can
+  #pseudocode: start form the first cell, keep jumping to the
+  #adjacent cell with minimum cost. if you hit a wall, then you can
   #either go down or sideways. Matrix movement interpretation:
   #
   # * diagonal = replace
@@ -48,36 +48,34 @@ class EditDistance
     i,j = 0,0,0,0
     while (i < l.size || j < t.size ) 
       op,min,c1,c2,c3 = :nop,0,nil,nil,nil
-      if i == (l.size-1) && (j == t.size-1)
-        op = :replace if table[i][j] != 0
-        min,i,j = 1,i+1,j+1 
+      if i == (l.size-1) && (j == t.size-1) && table[i][j] != 0
+        op = :replace
+        min,i2,j2 = 1,i+1,j+1 
       elsif j == t.size
-        op,i,j = :delete,i+1,j 
+        op,i2,j2 = :delete,i+1,j 
       elsif i == l.size 
-        op,i,j = :insert,i,j+1
+        op,i2,j2 = :insert,i,j+1
       else
         c0 = table[i][j]
-        c1 = c0 - table[i+1][j+1] if !table[i+1].nil? && !table[i+1][j+1].nil?  #replace
-        c2 = c0 - table[i+1][j]  if !table[i+1].nil? && !table[i+1][j].nil? #delete
-        c3 = c0 - table[i][j+1] if  !table[i].nil? && !table[i][j+1].nil?  #insert
-        if c1 == 0
-          op,i2,j2 = :nop,i+1,j+1
-        elsif c2 == 0
-          i2,j2 = i+1,j
-        elsif c3 == 0
-          i2,j2 = i,j+1
-        else
-          min = c0
-          op,i2,j2 = :nop,i+1,j+1
-          min,op,i2,j2 = 1,:replace,i+1,j+1 if !c1.nil? && 1 <= min
-          min,op,i2,j2 = 2,:delete,i+1,j if  !c2.nil? && 2 <= min
-          min,op,i2,j2 = 3,:insert,i,j+1 if !c3.nil? && 3 <= min
+        c1 = table[i+1][j+1] if !table[i+1].nil? && !table[i+1][j+1].nil?  #replace
+        c2 = table[i+1][j]  if !table[i+1].nil? && !table[i+1][j].nil? #delete
+        c3 = table[i][j+1] if !table[i].nil? && !table[i][j+1].nil?  #insert
+        op,min,i2,j2 = :replace,c0,i+1,j+1
+        min,op,i2,j2 = c1,:replace,i+1,j+1 if !c1.nil? && c1 <= min && (c0 - c1 == 1)
+        min,op,i2,j2 = c2,:delete,i+1,j if  !c2.nil? && c2 <= min && (c0 - c2 == 2)
+        min,op,i2,j2 = c3,:insert,i,j+1 if !c3.nil? && c3 <= min && (c0 - c3 == 3)
+        if min == c0
+          if op == :replace || op == :delete || op == :insert
+            op = :nop
+          else
+            p "fatal: cannot find a destination cell" if $DEBUG
+            p ({:p => {:i => i, :j => j},:op => op, :min => min, :replace => c1, :delete => c2, :insert => c3}) if $DEBUG
+          end
         end
-        p ({:p => {:i => i, :j => j},:op => op, :min => min, :replace => c1, :delete => c2, :insert => c3}) if $DEBUG
-        i,j = i2,j2
       end
-      op = :nop if min <= 0
-      r = {:op => op, :str => transform(op,l,t,i,j,table)} if op != :op
+      p ({:p => {:i => i, :j => j},:op => op, :min => min, :replace => c1, :delete => c2, :insert => c3}) if $DEBUG
+        i,j = i2,j2
+      r = {:op => op, :str => transform(op,l,t,i,j,table)}
       res<< r if r[:op] != :nop
     end
     res
--- edit_distance/test/edit_distance_test.rb
+++ edit_distance/test/edit_distance_test.rb
@@ -6,16 +6,22 @@ class EditDistanceTest < Test::Unit::TestCase
     @ed = EditDistance.new
   end
   def test_edit_distance
+    l = "foo"
+    t = "bar"
+    r = @ed.edit_distance_wrapper(l,t)
+    r[:steps]
+    assert_equal(3,r[:cost])
+
     l = "foo1"
     t = "baz"
     r = @ed.edit_distance_wrapper(l,t)
-    r[:steps].each {|x| p x} #if $DEBUG
+    p r[:steps]
     assert_equal(5,r[:cost])
 
     l = "abcdefghijkl"
     t = "bcdefghxlm"
     r = @ed.edit_distance_wrapper(l,t)
-    r[:steps].each {|x| p x} #if $DEBUG
+    p r[:steps]
     assert_equal(7,r[:cost])
 
     l = "abrackadbrea"
@@ -23,6 +29,13 @@ class EditDistanceTest < Test::Unit::TestCase
     p({:l => l, :t => t}) if $DEBUG
     r = @ed.edit_distance_wrapper(l,t)
     assert_equal(5,r[:cost])
-    r[:steps].each {|x| p x} #if $DEBUG
+    p r[:steps]
+    l = "xtesting"
+    t = "testings"
+    p({:l => l, :t => t}) if $DEBUG
+    r = @ed.edit_distance_wrapper(l,t)
+    assert_equal(5,r[:cost])
+    p r[:steps]
+
   end
 end
